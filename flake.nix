57f9f705feeda40f1ced517bbba4a2c146b80f3f
ilioscio <ilioscio@gmail.com>
2024-11-30 23:33:26 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,45 @@
 {
-  inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs";
-    };
-  };
+  description = "JavaScript scripts using Bun runtime";
 
-  outputs = { self, nixpkgs }: {
-    default = {
-      inputs = self.inputs;
-      outputs = { self, nixpkgs }: {
-        arguments = {
-          buildInputs = pkgs: with pkgs; [
-            go
-          ];
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-          src = ./.;
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-          buildCommand = "go build -o ./scripts/arguments.go";
-
-          package = {
-            pname = "arguments";
-            installPhase = ''
-              mkdir -p $out/bin
-              cp -r . $out
-            '';
+          arguments = {
+            buildInputs = pkgs: with pkgs; [
+              go
+            ];
+            src = ./.;
+            buildCommand = "go build -o ./scripts/arguments.go";
+            package = {
+              pname = "arguments";
+              installPhase = ''
+                mkdir -p $out/bin
+                cp -r . $out
+              '';
+            };
           };
+  
+        in
+        {
+          default = arguments;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        arguments = {
+          type = "app";
+          program = "${self.packages.${system}.arguments}/bin/arguments";
         };
-      };
+        default = self.apps.${system}.arguments;
+      });
     };
-  };
 }
